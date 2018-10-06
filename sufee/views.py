96b9f441327e0a2a76183459fc0c048340f8dc91
Stu Ryan <mikeryan56@gmail.com>
2018-10-05 23:32:37 -0400

    update of page-login

--- sufee/views.py
+++ sufee/views.py
@@ -9,43 +9,14 @@ def index():
     template = 'index.html'
     return render_template(template)
 
+@app.route("/login")
+def login():
+    template = "page-login.html"
+    return render_template(template)
+
+
 # @app.route("/about")
 # def aboutindex():
 #     template = 'about.html'
 #     return render_template(template)
 #
-# @app.route("/azuredeploy")
-# def azureindex():
-#     template = 'azuredeploy.html'
-#     return render_template(template)
-#
-#
-# @app.route("/riots")
-# def riots_index():
-#     template = 'riots.html'
-#     object_list = tutorials.fileimp.get_csv()
-#     return render_template(template, object_list=object_list)
-#
-# @app.route('/riots/<row_id>/')
-# def riots_detail(row_id):
-#     template = 'riots_detail.html'
-#     object_list = tutorials.fileimp.get_csv()
-#     for row in object_list:
-#         if row['id'] == row_id:
-#             return render_template(template, object=row)
-#     abort(404)
-#
-# @app.route("/buildings")
-# def buildings_index():
-#     template = 'buildings.html'
-#     object_buildinglist = tutorials.fileimp.get_buildingscsv()
-#     return render_template(template, object_buildinglist=object_buildinglist)
-#
-# @app.route('/buildings/<row_id>/')
-# def buildings_detail(row_id):
-#     template = 'buildings_detail.html'
-#     object_buildinglist = tutorials.fileimp.get_buildingscsv()
-#     for row in object_buildinglist:
-#         if row['id'] == row_id:
-#             return render_template(template, object=row)
-#     abort(404)
